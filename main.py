7f55532fbc5c4ca68d51ad041c84c9e762cbc92d
Rachele Edwards <dot.com1472@gmail.com>
2020-07-27 11:41:56 -0600

    setup for main page, import pygame and set colors, and screen

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+import os
+
+os.environ["DSL_VIDEO_CENTERED"]='1'
+
+WIDTH, HEIGHT = 1440, 900
+size = (WIDTH, HEIGHT)
+
+pygmae.init()
+pygame.display.set_caption()
+screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+fps = 60
+
+black = ()
+blue = ()
+white = (255, 255, 255)
+
