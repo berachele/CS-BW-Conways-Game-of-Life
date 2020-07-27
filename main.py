6c22d9baf280a7c724930bec86f3152eca62090f
Rachele Edwards <dot.com1472@gmail.com>
2020-07-27 12:11:09 -0600

    finished setup with main for while loop--not rendering yet, getting pygame error

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import os
+import grid
 
 os.environ["SDL_VIDEO_CENTERED"]='1'
 
@@ -16,3 +17,22 @@ black = (0, 0, 0)
 blue = (0, 14, 71)
 white = (255, 255, 255)
 
+scaler = 40
+offset = 1
+
+Grid = grid.Grid(width, height, scaler, offset)
+Grid.random2d_array()
+
+run = True
+
+while run:
+    clock.tick(fps)
+    screen.fill(black)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT():
+            run = False
+
+    Grid.Conway(off_color=white, on_color=blue, surface=screen)
+
+    pygame.display.update()
